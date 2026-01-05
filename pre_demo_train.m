b56dd4c876a1bff0e5b701fce2e395750704312e
Logan D <149882518+logandharuman@users.noreply.github.com>
2026-01-05 15:18:27 +0530

    Update pre_demo_train.m

--- pre_demo_train.m
+++ pre_demo_train.m
@@ -1,41 +1,35 @@
 clc; clear;
-
-fs = 100e3;
-N  = 4096;
-t  = (0:N-1)/fs;
-
-mods = ["AM","FM","PM","DSBSC","USB","LSB","NOISE"];
+mods = ["AM","DSBSC","FM","CARRIER","DIGITAL","NOISE","PM"];
 X = []; Y = [];
+fs_list = [50e3, 100e3, 200e3]; % Varying Sampling Rates
 
-for m = mods
-    for k = 1:50
-        x = gen_signal(m, fs, t);
-        x = add_awgn(x, 12);      % HIGH SNR
-        f = extract_features_iq(x, fs);
-        X = [X; f];
-        Y = [Y; m];
+for fs = fs_list
+    t = (0:4095)/fs;
+    for m = mods
+        fprintf("Training %s at fs=%.0f...\n", m, fs);
+        for k = 1:500 % High volume for variety
+            x = gen_signal(m, fs, t);
+            
+            % FOCUS on the 0-10dB region specifically
+            if m ~= "NOISE"
+                snr_train = 0 + 15*rand(); % Random SNR between 0 and 15 dB
+                x = add_awgn(x, snr_train);
+            end
+            
+            f = extract_features_iq(x, fs, 1);
+            X = [X; f];
+            Y = [Y; m];
+        end
     end
 end
 
+% Manual Standardization
 Y = categorical(Y);
-
-% --- MANUAL STANDARDIZATION ---
-mu = mean(X,1);
-sigma = std(X,[],1) + eps;
+mu = mean(X, 1); sigma = std(X, [], 1) + eps;
 Xn = (X - mu) ./ sigma;
 
-% --- MULTI-CLASS SVM via ECOC ---
-tSVM = templateSVM('KernelFunction','rbf');
+% Use a weighted RBF SVM for better low-SNR boundaries
+tSVM = templateSVM('KernelFunction', 'rbf', 'KernelScale', 'auto');
+SVM = fitcecoc(Xn, Y, 'Learners', tSVM, 'Coding', 'onevsall');
 
-SVM = fitcecoc(Xn, Y, ...
-    'Learners', tSVM, ...
-    'Coding','onevsone');
-
-% --- CROSS VALIDATION ---
-CVSVM = crossval(SVM,'KFold',5);
-cvLoss = kfoldLoss(CVSVM);
-cvAccuracy = (1-cvLoss)*100;
-
-fprintf("Cross-Validated Accuracy: %.2f %%\n", cvAccuracy);
-
-save('AMC_MODEL.mat','SVM','mu','sigma','fs','N','cvAccuracy');
+save('AMC_MODEL.mat', 'SVM', 'mu', 'sigma');
